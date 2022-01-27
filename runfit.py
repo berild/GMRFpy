d6abbf04d9646540e449507ae83c67303c38cc67
berild <moutzebe@gmail.com>
2022-01-27 13:24:33 +0100

    fix check

--- runfit.py
+++ runfit.py
@@ -27,18 +27,17 @@ def fitPar(model,data):
 
 def findFits(model, data):
     vers = np.array([[i,j,k] for i in range(0,100) for j in range(1,4) for k in range(1,4)])
-    modstr = ["SI", "SA", "NA"]
-    dho = ["100","10000","27000"]
-    r = ["1","10","100"]
+    modstr = np.array(["SI", "SA", "NA"])
+    dho = np.array(["100","10000","27000"])
+    r = np.array(["1","10","100"])
     mods = []
     print(vers.shape)
     for file in os.listdir("./fits/"):
         if file.startswith(modstr[model-1]+"-"+modstr[data-1]):
             tmp = file.split('-')[2:]
-            print(np.where(dho == tmp[0][3:]))
-            tdho = np.where(dho == tmp[0][3:])[0] + 1
+            tdho = np.where(dho == tmp[0][3:])[0][0] + 1
             tnum = int(tmp[2].split(".")[0])
-            tr = np.where(r==tmp[1][1:])[0]+1
+            tr = np.where(r==tmp[1][1:])[0][0]+1
             tar = np.array([tnum,tdho,tr]) # num , dho , r
             vers = np.delete(vers,np.where((vers == tar).all(axis=1))[0],axis=0)
     print(vers.shape)
