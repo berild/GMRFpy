c5d92cf2bf676f095b8c15a15fecbee8c81fd98a
berild <moutzebe@gmail.com>
2022-01-28 12:30:28 +0100

    change verbose of parallel

--- runfit.py
+++ runfit.py
@@ -20,7 +20,7 @@ def fitPar(model,data):
     vers = findFits(model,data)
     mod = spde(model = model)
     fit_ = partial(fit, mod=mod, data = data, vers=vers)
-    res = Parallel(n_jobs=40,verbose = 10)(delayed(fit_)(i) for i in range(vers.shape[0]))
+    res = Parallel(n_jobs=40,verbose = 100)(delayed(fit_)(i) for i in range(vers.shape[0]))
     return(res)
 
 def findFits(model, data):
