0e64f9dd27288bb0d56370a166e022a3361d2467
berild <moutzebe@gmail.com>
2022-02-08 15:36:17 +0100

    add fifth model

--- runfit.py
+++ runfit.py
@@ -25,7 +25,7 @@ def fitPar(model,data):
 
 def findFits(model, data):
     vers = np.array([[i,j,k] for i in range(1,101) for j in range(1,4) for k in range(1,4)])
-    modstr = np.array(["SI", "SA", "NA1"])
+    modstr = np.array(["SI", "SA", "NA1","NA2","SA1"])
     dho = np.array(["100","10000","27000"])
     r = np.array(["1","10","100"])
     for file in os.listdir("./fits/"):
@@ -39,7 +39,7 @@ def findFits(model, data):
     return(vers)
 
 def main(argv):
-    modstr = ["Stationary Isotropic", "Stationary Anistropic", "Non-stationary Simple Anisotropic","Non-stationary Complex Anisotropic"]
+    modstr = ["Stationary Isotropic", "Stationary Anistropic", "Non-stationary Simple Anisotropic","Non-stationary Complex Anisotropic","Stationary Anistropic new"]
     mods = None
     if "-" in argv[0]:
         mods = argv[0].split('-',2)
