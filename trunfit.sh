2a5a4ac5c3aa12ad380aae9e3b979b460dfd3836
berild <moutzebe@gmail.com>
2022-02-10 17:37:50 +0100

    add terminal script

---
+++ trunfit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Ask the user for their name
+echo Which model to run?
+read varname1
+echo From which number?
+read varname2
+echo Running model $varname1 from number $varname2
+source ../env/bin/activate
+python3 srunfit.py $varname1 $varname2
