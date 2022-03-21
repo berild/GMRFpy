96641c5747eb92a6cc250c0804f794fc1e4821f2
berild <moutzebe@gmail.com>
2022-03-21 12:23:41 +0100

    try fix

--- trunfit.sh
+++ trunfit.sh
@@ -5,5 +5,5 @@ read varname1
 echo From which number?
 read varname2
 echo Running model $varname1 from number $varname2
-source "../env/bin/activate"
+source ../env/bin/activate
 python3 srunfit.py $varname1 $varname2
