6df10be46430aef2fcc182f8875d8b504e51a950
berild <moutzebe@gmail.com>
2022-03-21 12:26:44 +0100

    try fix

--- trunfit.sh
+++ trunfit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Ask the user for their name
-echo Which model to run?
-read varname1
-echo From which number?
-read varname2
-echo Running model $varname1 from number $varname2
+#echo Which model to run?
+#read varname1
+#echo From which number?
+#read varname2
+#echo Running model $varname1 from number $varname2
 
 source ../env/bin/activate
 
-python3 srunfit.py $varname1 $varname2
+#python3 srunfit.py $varname1 $varname2
