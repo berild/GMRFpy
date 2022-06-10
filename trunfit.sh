b7f95c5c5ff58d4d316a7530c2216c88c81323e3
berild <moutzebe@gmail.com>
2022-06-10 11:25:12 +0200

    fix

--- trunfit.sh
+++ trunfit.sh
@@ -11,6 +11,6 @@ module load R/4.1.0-foss-2021a
 module load CMake/3.20.1-GCCcore-10.3.0
 module load SuiteSparse/5.10.1-foss-2021a-METIS-5.1.0
 module load NLopt/2.7.0-GCCcore-10.3.0
-source ../pyenb/bin/activate
+source ../pyenv/bin/activate
 
 python3 srunfit.py $varname1 $varname2
