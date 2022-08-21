5b74c0faafe6588a2cb70d89ffbaff3a4e7f2470
berild <moutzebe@gmail.com>
2022-08-21 11:42:08 +0200

    fix memory issue

--- StatIso.py
+++ StatIso.py
@@ -1,4 +1,3 @@
-import sys, getopt
 import numpy as np
 from scipy import sparse
 from ah3d2 import AH
@@ -9,6 +8,7 @@ importr("Matrix")
 from scipy.optimize import minimize
 import os
 from grid import Grid
+robj.r.source("rqinv.R")
 
 def delete_rows_csr(mat, indices):
     """
@@ -64,6 +64,10 @@ class StatIso:
         self.iDv = 1/self.V*sparse.eye(self.grid.M*self.grid.N*self.grid.P)
         self.grid.basisH()
         self.grid.basisN()
+
+    def getPars(self):
+        return(np.hstack([self.kappa,self.gamma,self.tau]))
+
         
     def load(self,simple = False):
         if self.loaded:
@@ -76,37 +80,30 @@ class StatIso:
         if not simple:
             Hs = np.exp(self.gamma)*np.eye(3) + np.zeros((self.n,6,3,3))
             Dk =  sparse.diags([np.exp(self.kappa)]*self.n) 
-            A_H = AH(self.grid.M,self.grid.N,self.grid.P,Hs,self.grid.hx,self.grid.hy,self.grid.hz)
-            Ah = sparse.csc_matrix((A_H.Val(), (A_H.Row(), A_H.Col())), shape=(self.n, self.n))
-            A_mat = self.Dv@Dk - Ah
+            A_mat = self.Dv@Dk - AH(self.grid.M,self.grid.N,self.grid.P,Hs,self.grid.hx,self.grid.hy,self.grid.hz)
             self.Q = A_mat.transpose()@self.iDv@A_mat
-            self.Q_fac = cholesky(self.Q)
+            self.Q_fac = self.cholesky(self.Q)
             self.mvar = rqinv(self.Q).diagonal()
             self.loaded = True
     
-    # maybe add some assertions
-    def loadFit(self, simmod, dho, r, num, file = None):
-        if file is None:
-            mods = np.array(['SI','SA','NA1','NA2'])
-            dhos = np.array(['100','10000','27000'])
-            rs = np.array([1,10,100])
-            file = './fits/' + mods[simmod-1] + '-SI-dho' + dhos[dho-1] + '-r' + str(rs[r-1]) + '-' + str(num) +'.npz'
-            print(file)
-        fitmod = np.load(file)
-        self.S = fitmod['S']*1
-        par =fitmod['par']*1
-        self.kappa = par[0]
-        self.gamma = par[1]
-        self.tau = par[2]
+    def fit(self,data, r, S, par = None,verbose = False, grad = True):
+        if par is None:
+            par = np.array([-1,0.5,2])
+        self.data = data
+        self.r = r  
+        self.S = S
+        self.opt_steps = 0
+        self.grad = grad
+        self.verbose = verbose
+        if self.grad:
+            res = minimize(self.logLike, x0 = par,jac = True, method = "BFGS",tol = 1e-4)
+        else:    
+            res = minimize(self.logLike, x0 = par, tol = 1e-4)
+        self.kappa = res['x'][0]
+        self.gamma = res['x'][1]
+        self.tau = res['x'][2]
         self.sigma = np.log(np.sqrt(1/np.exp(self.tau)))
-        Hs = np.exp(par[1])*np.eye(3) + np.zeros((self.n,6,3,3))
-        Dk =  sparse.diags([np.exp(par[0])]*self.n) 
-        A_H = AH(self.grid.M,self.grid.N,self.grid.P,Hs,self.grid.hx,self.grid.hy,self.grid.hz)
-        Ah = sparse.csc_matrix((A_H.Val(), (A_H.Row(), A_H.Col())), shape=(self.n,self.n))
-        A_mat = self.Dv@Dk - Ah
-        self.Q = A_mat.transpose()@self.iDv@A_mat
-        self.Q_fac = cholesky(self.Q)
-        self.mvar = rqinv(self.Q).diagonal()
+        return(res)
 
     def fitTo(self,simmod,dho,r,num,verbose = False, grad = True, par = None):
         if par is None:
@@ -122,40 +119,84 @@ class StatIso:
         self.S = sparse.diags(self.S)
         self.S =  delete_rows_csr(self.S.tocsr(),np.where(self.S.diagonal() == 0))
         res = self.fit(data = self.data, r=self.r, S = self.S,verbose = verbose, grad = grad,par = par)
-        np.savez(file = './fits/' + mods[simmod-1] + '-SI-dho' + dhos[dho-1] + '-r' + str(rs[r-1]) + '-' + str(num) +'.npz', par = res['x'], S = self.S)
+        np.savez(file = './fits/' + mods[simmod-1] + '-SI-dho' + dhos[dho-1] + '-r' + str(rs[r-1]) + '-' + str(num) +'.npz', par = res['x'])
         return(True)
 
-    # implement S either locations, the array or not specified but data must be nans
-    def fit(self,data, r, S = None, par = None,verbose = False, grad = True):
-        if par is None:
-            par = np.array([-1,0.5,2])
-        assert S is not None
-        self.data = data
-        self.r = r  
-        self.S = S
-        self.opt_steps = 0
-        self.grad = grad
-        self.verbose = verbose
-        if self.grad:
-            res = minimize(self.logLike, x0 = par,jac = True, method = "BFGS",tol = 1e-3)
-        else:    
-            res = minimize(self.logLike, x0 = par, tol = 1e-3)
-        self.kappa = res['x'][0]
-        self.gamma = res['x'][1]
-        self.tau = res['x'][2]
+    def loadFit(self, simmod, dho, r, num, file = None):
+        if file is None:
+            mods = np.array(['SI','SA','NA1','NA2'])
+            dhos = np.array(['100','10000','27000'])
+            rs = np.array([1,10,100])
+            file = './fits/' + mods[simmod-1] + '-SI-dho' + dhos[dho-1] + '-r' + str(rs[r-1]) + '-' + str(num) +'.npz'
+            print(file)
+        fitmod = np.load(file)
+        self.S = sparse.eye(self.n)
+        par =fitmod['par']*1
+        self.kappa = par[0]
+        self.gamma = par[1]
+        self.tau = par[2]
         self.sigma = np.log(np.sqrt(1/np.exp(self.tau)))
-        return(res)
+        Hs = np.exp(par[1])*np.eye(3) + np.zeros((self.n,6,3,3))
+        Dk =  sparse.diags([np.exp(par[0])]*self.n) 
+        A_mat = self.Dv@Dk - AH(self.grid.M,self.grid.N,self.grid.P,Hs,self.grid.hx,self.grid.hy,self.grid.hz)
+        self.Q = A_mat.transpose()@self.iDv@A_mat
+        self.Q_fac = cholesky(self.Q)
+        self.mvar = rqinv(self.Q).diagonal()
 
-    def getPars(self):
-        return(np.hstack([self.kappa,self.gamma,self.tau]))
+    def sample(self,n = 1, par = None):
+        if par is None:
+            assert(self.kappa is not None and self.gamma is not None and self.sigma is not None)
+        if self.Q is None or self.Q_fac is None:
+            self.setQ(par = par)
+        data = np.zeros((self.n,n))
+        for i in range(n):
+            z = np.random.normal(size = self.n)
+            data[:,i] = self.Q_fac.apply_Pt(self.Q_fac.solve_Lt(z,use_LDLt_decomposition=False)) + np.random.normal(size = self.n)*np.exp(self.sigma)
+        return(data)
+
+    def sim(self):
+        self.load()
+        mods = []
+        for file in os.listdir("./simulations/"):
+            if file.startswith("SI-"):
+                mods.append(int(file.split("-")[1].split(".")[0]))
+        if not mods:
+            num = 1
+        else:
+            num = max(mods) + 1 
+        self.data = self.sample(n=100)
+        np.savez('./simulations/SI-'+ str(num) +'.npz', data = self.data, locs100 = np.random.choice(np.arange(self.n), 100, replace = False), locs10000 = np.random.choice(np.arange(self.n), 10000, replace = False), locs27000 = np.arange(self.n))
+        return(True)
+
+    def setQ(self,par = None):
+        if par is None:
+            assert(self.kappa is not None and self.gamma is not None and self.sigma is not None)
+        else:
+            self.kappa = par[0]
+            self.gamma = par[1]
+            self.sigma = par[2]
+        Hs = np.exp(self.gamma)*np.eye(3) + np.zeros((self.n,6,3,3))
+        Dk =  sparse.diags([np.exp(self.kappa)]*self.n) 
+        A_mat = self.Dv@Dk - AH(self.grid.M,self.grid.N,self.grid.P,Hs,self.grid.hx,self.grid.hy,self.grid.hz)
+        self.Q = A_mat.transpose()@self.iDv@A_mat
+        self.Q_fac = cholesky(self.Q)
+
+
+    def cholesky(self,Q):
+        try: 
+            Q_fac = cholesky(Q)
+        except:
+            print("Supernodal or negative definite precision matrix... continue")
+            return(-1)
+        else:
+            return(Q_fac)
+        
 
     def logLike(self, par):
         data  = self.data
         Hs = np.exp(par[1])*np.eye(3) + np.zeros((self.n,6,3,3))
         Dk =  np.exp(par[0])*sparse.eye(self.n) 
-        A_H = AH(self.grid.M,self.grid.N,self.grid.P,Hs,self.grid.hx,self.grid.hy,self.grid.hz)
-        Ah = sparse.csc_matrix((A_H.Val(), (A_H.Row(), A_H.Col())), shape=(self.n, self.n))
-        A_mat = self.Dv@Dk - Ah
+        A_mat = self.Dv@Dk - AH(self.grid.M,self.grid.N,self.grid.P,Hs,self.grid.hx,self.grid.hy,self.grid.hz)
         Q = A_mat.transpose()@self.iDv@A_mat
         Q_c = Q + self.S.transpose()@self.S*np.exp(par[2])
         Q_fac = self.cholesky(Q)
@@ -174,7 +215,7 @@ class StatIso:
             Qcinv = rqinv(Q_c)
 
             A_kappa = Dk@self.Dv
-            A_gamma = - Ah
+            A_gamma = - AH(self.grid.M,self.grid.N,self.grid.P,Hs,self.grid.hx,self.grid.hy,self.grid.hz)
 
             Q_kappa = A_kappa.transpose()@self.iDv@A_mat + A_mat.transpose()@self.iDv@A_kappa
             Q_gamma = A_gamma.transpose()@self.iDv@A_mat +  A_mat.transpose()@self.iDv@A_gamma
@@ -208,55 +249,3 @@ class StatIso:
             if self.verbose:
                 print("# %4.0f"%self.opt_steps," log-likelihood = %4.4f"%(-like), "\u03BA = %2.2f"%np.exp(par[0]), "\u03B3 = %2.2f"%np.exp(par[1]), "\u03C3 = %2.2f"%np.sqrt(1/np.exp(par[2])))
             return(like)
-
-    def setQ(self,par = None):
-        if par is None:
-            assert(self.kappa is not None and self.gamma is not None and self.sigma is not None)
-        else:
-            self.kappa = par[0]
-            self.gamma = par[1]
-            self.sigma = par[2]
-        Hs = np.exp(self.gamma)*np.eye(3) + np.zeros((self.n,6,3,3))
-        Dk =  sparse.diags([np.exp(self.kappa)]*self.n) 
-        A_H = AH(self.grid.M,self.grid.N,self.grid.P,Hs,self.grid.hx,self.grid.hy,self.grid.hz)
-        Ah = sparse.csc_matrix((A_H.Val(), (A_H.Row(), A_H.Col())), shape=(self.n, self.n))
-        A_mat = self.Dv@Dk - Ah
-        self.Q = A_mat.transpose()@self.iDv@A_mat
-        self.Q_fac = cholesky(self.Q)
-        
-
-    def sim(self):
-        self.load()
-        mods = []
-        for file in os.listdir("./simulations/"):
-            if file.startswith("SI-"):
-                mods.append(int(file.split("-")[1].split(".")[0]))
-        if not mods:
-            num = 1
-        else:
-            num = max(mods) + 1 
-        self.data = self.sample(n=100)
-        np.savez('./simulations/SI-'+ str(num) +'.npz', data = self.data, locs100 = np.random.choice(np.arange(self.n), 100, replace = False), locs10000 = np.random.choice(np.arange(self.n), 10000, replace = False), locs27000 = np.arange(self.n))
-        return(True)
-        
-
-    def sample(self,n = 1, par = None):
-        if par is None:
-            assert(self.kappa is not None and self.gamma is not None and self.sigma is not None)
-        if self.Q is None or self.Q_fac is None:
-            self.setQ(par = par)
-        data = np.zeros((self.n,n))
-        for i in range(n):
-            z = np.random.normal(size = self.n)
-            data[:,i] = self.Q_fac.apply_Pt(self.Q_fac.solve_Lt(z,use_LDLt_decomposition=False)) + np.random.normal(size = self.n)*np.exp(self.sigma)
-        return(data)
-    
-    def cholesky(self,Q):
-        try: 
-            Q_fac = cholesky(Q)
-        except:
-            print("Supernodal or negative definite precision matrix... continue")
-            return(-1)
-        else:
-            return(Q_fac)
-            
