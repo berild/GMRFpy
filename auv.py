f881ad36b0158f13464e9f64979539bfbb1992ba
berild <moutzebe@gmail.com>
2022-08-23 08:44:37 +0200

    fix issues with loadKeep()

--- auv.py
+++ auv.py
@@ -20,20 +20,18 @@ class auv:
         """
         self.spde = spde
         Q = spde.mod.Q.copy()
+        self.n = spde.mod.n
+        Stot = sparse.eye(self.n)
         self.mu = mu
         self.mu2 = np.hstack([np.zeros(self.n),0,1]).reshape(-1,1) 
         self.mu3 = mu
-        Stot = spde.mod.S.copy()
-        self.n = spde.mod.n
 
-        Q = Q.tocsc()
         Q.resize((self.n+2,self.n+2))
         Q = Q.tolil()
-        Q[self.n,self.n] = 0.02
-        Q[self.n+1,self.n+1] = 10
-        self.Q = Q
+        Q[self.n,self.n] = 100
+        Q[self.n+1,self.n+1] = 100
+        self.Q = Q.tocsc()
         
-        Stot = Stot.tocsc()
         Stot.resize((self.n,self.n+2))
         Stot = Stot.tolil()
         Stot[:,self.n] = np.ones(self.n)
@@ -62,13 +60,13 @@ class auv:
             rel ([k,1]-array): k number of measurements of the GMRF. (k>0).
             ks ([k,]-array): k number of indicies describing the index of the measurment in the field. 
         """
-        ks = data['idx']
-        rel = data['data'].reshape(-1,1)
+        ks = data['idx'].to_numpy()
+        rel = data['data'].to_numpy().reshape(-1,1)
         if ks.size>0:
             S = self.Stot[ks,:]
-            self.Q = self.Q + S.transpose()@S*1/self.sigma[0]**2
+            self.Q = self.Q + S.transpose()@S*1/self.sigma**2
             self.Q_fac.cholesky_inplace(self.Q)
-            self.mu2 = self.mu2 - self.Q_fac.solve_A(S.transpose().tocsc())@(S@self.mu2 - rel)*1/self.sigma[0]**2
+            self.mu2 = self.mu2 - self.Q_fac.solve_A(S.transpose().tocsc())@(S@self.mu2 - rel)*1/self.sigma**2
             self.mu = self.mu2[:self.n,0] + self.mu2[self.n,0] + self.mu3*self.mu2[self.n+1,0]
         if keep:
             self.Q_k = self.Q.copy()
@@ -77,9 +75,9 @@ class auv:
 
     def loadKeep(self):
         self.Q = self.Q_k.copy()
-        self.mu2 = self.mu2
-        self.mu_k = self.mu
-        self.Q_fac.cholesky_inplace(self.Q)
+        self.mu2 = self.mu2_k
+        self.mu = self.mu_k
+        self.Q_fac.cholesky_inplace(self.Q_k)
 
     def mvar(self, simple = False, n = DEFAULT_NUM_SAMPLES):
         if not simple:
@@ -91,4 +89,4 @@ class auv:
             tmpQinv =  np.array(robj.r.rqinv(robj.r["sparseMatrix"](i = robj.FloatVector(r+1),j = robj.FloatVector(c+1),x = robj.FloatVector(v))))
             return(sparse.csc_matrix((np.array(tmpQinv[:,2],dtype = "float32"), (np.array(tmpQinv[:,0],dtype="int32"), np.array(tmpQinv[:,1],dtype="int32"))), shape=tshape).diagonal()+self.sigma**2)
         else:
-            return(self.sample(n=DEFAULT_NUM_SAMPLES).var())
+            return(self.sample(n=DEFAULT_NUM_SAMPLES).var(axis = 1))
