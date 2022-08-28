5b25dc7359a29f55fc3cbb76bef106ded3f9efdf
berild <moutzebe@gmail.com>
2022-08-28 21:12:58 +0200

    add model specification to fit

--- fitEmulator.py
+++ fitEmulator.py
@@ -1,13 +1,14 @@
 from mission import mission
 import numpy as np
+import sys
 
-def main():
+def main(argv):
     mis = mission('nidelva_27_05_21')
-    mis.emulator(model=4,pars = True)
+    mis.emulator(model=int(argv[0]),pars = True)
     mis.mean(version2 = "tfc")
     par = np.hstack([[-1.1]*27,[-1.2]*27,[5.1]*27,[4.1]*27,[0.04]*27,[0.4]*27,[0.6]*27,5])
     mis.fit(par= par, verbose = True)
     print("finished")
 
 if __name__ == "__main__":
-   main()
+   main(sys.argv[1:])
