c9ec2bd58e1555ba2178533d3262003468248935
berild <moutzebe@gmail.com>
2022-09-01 14:27:47 +0200

    change commits

--- NonStatAnIsoN.py
+++ NonStatAnIsoN.py
@@ -80,8 +80,6 @@ class NonStatAnIso:
         return(np.hstack([self.kappa,self.gamma,self.vx,self.vy,self.vz,self.rho1,self.rho2,self.tau]))
 
     def load(self,simple = False):
-        if self.loaded:
-            return
         simmod = np.load("./simmodels/NA.npz")
         self.kappa = simmod['kappa']*1
         self.gamma = simmod['gamma']*1
@@ -99,7 +97,6 @@ class NonStatAnIso:
         if not simple:
             self.Q_fac = self.cholesky(self.Q)
             assert(self.Q_fac != -1)
-            self.loaded = True
 
     def fit(self,data, r, S,verbose = False, fgrad = True, par = None):
         #mod4: kappa(0:27), gamma(27:54), vx(54:81), vy(81:108), vz(108:135), rho1(135:162), rho2(162:189), sigma(189)
@@ -177,18 +174,22 @@ class NonStatAnIso:
         self.Q_fac = self.cholesky(self.Q)
         self.mvar = rqinv(self.Q).diagonal()
 
-    def sample(self,n = 1, Q_fac = None, sigma = None, simple = False):
-        if Q_fac is None:
-            Q_fac = self.Q_fac
+    # def sample(self,n = 1, Q_fac = None, sigma = None, simple = False):
+    #     if Q_fac is None:
+    #         Q_fac = self.Q_fac
+    #     z = np.random.normal(size = self.n*n).reshape(self.n,n)
+    #     if simple:
+    #         data = Q_fac.apply_Pt(Q_fac.solve_Lt(z,use_LDLt_decomposition=False)) 
+    #     else:
+    #         if sigma is None:
+    #             sigma = self.sigma 
+    #         data = Q_fac.apply_Pt(Q_fac.solve_Lt(z,use_LDLt_decomposition=False)) + np.random.normal(size = self.n*n).reshape(self.n,n)*np.exp(sigma)
+    #     if n ==1:
+    #         data = data.reshape(self.n)
+    #     return(data)
+    def sample(self,n = 1):
         z = np.random.normal(size = self.n*n).reshape(self.n,n)
-        if simple:
-            data = Q_fac.apply_Pt(Q_fac.solve_Lt(z,use_LDLt_decomposition=False)) 
-        else:
-            if sigma is None:
-                sigma = self.sigma 
-            data = Q_fac.apply_Pt(Q_fac.solve_Lt(z,use_LDLt_decomposition=False)) + np.random.normal(size = self.n*n).reshape(self.n,n)*np.exp(sigma)
-        if n ==1:
-            data = data.reshape(self.n)
+        data = self.Q_fac.apply_Pt(self.Q_fac.solve_Lt(z,use_LDLt_decomposition=False)) + np.random.normal(size = self.n*n).reshape(self.n,n)*np.exp(self.sigma)
         return(data)
 
     def sim(self):
@@ -227,101 +228,6 @@ class NonStatAnIso:
         if not simple:
             self.Q_fac = self.cholesky(self.Q)
 
-    # def getH(self,gamma = None,vx = None,vy = None, vz = None,rho1 = None,rho2 = None,d=None,var = None):
-    #     if gamma is None and vx is None and vy is None and vz is None and rho1 is None and rho2 is None:
-    #         gamma = self.gamma
-    #         vx = self.vx
-    #         vy = self.vy
-    #         vz = self.vz
-    #         rho1 = self.rho1
-    #         rho2 = self.rho2
-    #     if var is None:
-    #         pg = np.exp(self.grid.evalBH(par = gamma))
-    #         vv = np.stack([self.grid.evalBH(par = vx),self.grid.evalBH(par = vy),self.grid.evalBH(par = vz)],axis=2)
-    #         vb1 = np.stack([-vv[:,:,1],vv[:,:,0],vv[:,:,2]*0],axis=2)/np.sqrt(vv[:,:,0]**2 + vv[:,:,1]**2)[:,:,np.newaxis]
-    #         vb2 = np.stack([-vv[:,:,2]*vv[:,:,0],-vv[:,:,2]*vv[:,:,1],vv[:,:,0]**2 + vv[:,:,1]**2],axis=2)/np.sqrt(vv[:,:,2]**2*vv[:,:,0]**2 + vv[:,:,2]**2*vv[:,:,1]**2+(vv[:,:,0]**2 + vv[:,:,1]**2)**2)[:,:,np.newaxis]
-    #         ww = vb1*self.grid.evalBH(par = rho1)[:,:,np.newaxis] + vb2*self.grid.evalBH(par = rho2)[:,:,np.newaxis]
-    #         H = (np.eye(3)*(np.stack([pg,pg,pg],axis=2))[:,:,:,np.newaxis]) + vv[:,:,:,np.newaxis]*vv[:,:,np.newaxis,:] + ww[:,:,:,np.newaxis]*ww[:,:,np.newaxis,:]
-    #     elif var == 0: #gamma
-    #         pg = np.exp(self.grid.evalBH(par = gamma))
-    #         H = np.eye(3)*(np.stack([self.grid.bsH[:,:,d]*pg,self.grid.bsH[:,:,d]*pg,self.grid.bsH[:,:,d]*pg],axis=2)[:,:,:,np.newaxis])
-    #     elif var == 1: #vx
-    #         dpar = np.zeros(27)
-    #         dpar[d] = 1
-    #         vv = np.stack([self.grid.evalBH(par = vx),self.grid.evalBH(par = vy),self.grid.evalBH(par = vz)],axis=2)
-    #         dv = np.stack([self.grid.evalBH(par = dpar),self.grid.evalBH(par = np.zeros(27)),self.grid.evalBH(par = np.zeros(27))],axis = 2)
-    #         vb1 = np.stack([-vv[:,:,1],vv[:,:,0],vv[:,:,2]*0],axis=2)/np.sqrt(vv[:,:,0]**2 + vv[:,:,1]**2)[:,:,np.newaxis]
-    #         vb2 = np.stack([-vv[:,:,2]*vv[:,:,0],-vv[:,:,2]*vv[:,:,1],vv[:,:,0]**2 + vv[:,:,1]**2],axis=2)/np.sqrt(vv[:,:,2]**2*vv[:,:,0]**2 + vv[:,:,2]**2*vv[:,:,1]**2+(vv[:,:,0]**2 + vv[:,:,1]**2)**2)[:,:,np.newaxis]
-    #         ww = vb1*self.grid.evalBH(par = rho1)[:,:,np.newaxis] + vb2*self.grid.evalBH(par = rho2)[:,:,np.newaxis]
-    #         c1 = (vv[:,:,0]**2 + vv[:,:,1]**2)**(3.0/2.0)
-    #         c2 = (vv[:,:,2]**2*vv[:,:,0]**2 + vv[:,:,2]**2*vv[:,:,1]**2+(vv[:,:,0]**2 + vv[:,:,1]**2)**2)**(3.0/2.0)
-    #         dvb1 = np.stack([vv[:,:,1]*vv[:,:,0],
-    #                          vv[:,:,1]**2,
-    #                          vv[:,:,2]*0],axis=2)*(dv[:,:,0]/c1)[:,:,np.newaxis]
-    #         dvb2 = np.stack([vv[:,:,2]*vv[:,:,0]**4 - vv[:,:,2]**3*vv[:,:,1]**2 - vv[:,:,2]*vv[:,:,1]**4,
-    #                          vv[:,:,2]**3*vv[:,:,0]*vv[:,:,1] + 2*vv[:,:,2]*vv[:,:,1]*vv[:,:,0]**3 + 2*vv[:,:,2]*vv[:,:,1]**3*vv[:,:,0],
-    #                          vv[:,:,2]**2*vv[:,:,0]**3 + vv[:,:,2]**2*vv[:,:,0]*vv[:,:,1]**2],axis=2)*(dv[:,:,0]/c2)[:,:,np.newaxis]
-    #         ww = vb1*self.grid.evalBH(par = rho1)[:,:,np.newaxis] + vb2*self.grid.evalBH(par = rho2)[:,:,np.newaxis]
-    #         dw = dvb1*self.grid.evalBH(par = rho1)[:,:,np.newaxis] + dvb2*self.grid.evalBH(par = rho2)[:,:,np.newaxis]
-    #         H = vv[:,:,:,np.newaxis]*dv[:,:,np.newaxis,:]  + dv[:,:,:,np.newaxis]*vv[:,:,np.newaxis,:] + dw[:,:,:,np.newaxis]*ww[:,:,np.newaxis,:] + ww[:,:,:,np.newaxis]*dw[:,:,np.newaxis,:]
-    #     elif var == 2: #vy
-    #         dpar = np.zeros(27)
-    #         dpar[d] = 1
-    #         vv = np.stack([self.grid.evalBH(par = vx),self.grid.evalBH(par = vy),self.grid.evalBH(par = vz)],axis=2)
-    #         dv = np.stack([self.grid.evalBH(par = np.zeros(27)),self.grid.evalBH(par = dpar),self.grid.evalBH(par = np.zeros(27))],axis = 2)
-    #         vb1 = np.stack([-vv[:,:,1],vv[:,:,0],vv[:,:,2]*0],axis=2)/np.sqrt(vv[:,:,0]**2 + vv[:,:,1]**2)[:,:,np.newaxis]
-    #         vb2 = np.stack([-vv[:,:,2]*vv[:,:,0],-vv[:,:,2]*vv[:,:,1],vv[:,:,0]**2 + vv[:,:,1]**2],axis=2)/np.sqrt(vv[:,:,2]**2*vv[:,:,0]**2 + vv[:,:,2]**2*vv[:,:,1]**2+(vv[:,:,0]**2 + vv[:,:,1]**2)**2)[:,:,np.newaxis]
-    #         ww = vb1*self.grid.evalBH(par = rho1)[:,:,np.newaxis] + vb2*self.grid.evalBH(par = rho2)[:,:,np.newaxis]
-    #         c1 = (vv[:,:,0]**2 + vv[:,:,1]**2)**(3.0/2.0)
-    #         c2 = (vv[:,:,2]**2*vv[:,:,0]**2 + vv[:,:,2]**2*vv[:,:,1]**2+(vv[:,:,0]**2 + vv[:,:,1]**2)**2)**(3.0/2.0)
-    #         dvb1 = np.stack([-vv[:,:,0]**2,
-    #                          -vv[:,:,0]*vv[:,:,1],
-    #                          vv[:,:,2]*0],axis=2)*(dv[:,:,1]/c1)[:,:,np.newaxis]
-    #         dvb2 = np.stack([vv[:,:,2]**3*vv[:,:,0]*vv[:,:,1] + 2*vv[:,:,2]*vv[:,:,0]**3*vv[:,:,1] + 2*vv[:,:,2]*vv[:,:,0]*vv[:,:,1]**3,
-    #                          vv[:,:,2]*vv[:,:,1]**4 - vv[:,:,2]**3*vv[:,:,0]**2 - vv[:,:,2]*vv[:,:,0]**4,
-    #                          vv[:,:,2]**2*vv[:,:,1]**3 + vv[:,:,2]**2*vv[:,:,0]**2*vv[:,:,1]],axis=2)*(dv[:,:,1]/c2)[:,:,np.newaxis]
-    #         ww = vb1*self.grid.evalBH(par = rho1)[:,:,np.newaxis] + vb2*self.grid.evalBH(par = rho2)[:,:,np.newaxis]
-    #         dw = dvb1*self.grid.evalBH(par = rho1)[:,:,np.newaxis] + dvb2*self.grid.evalBH(par = rho2)[:,:,np.newaxis]
-    #         H = vv[:,:,:,np.newaxis]*dv[:,:,np.newaxis,:]  + dv[:,:,:,np.newaxis]*vv[:,:,np.newaxis,:] + dw[:,:,:,np.newaxis]*ww[:,:,np.newaxis,:] + ww[:,:,:,np.newaxis]*dw[:,:,np.newaxis,:]
-    #     elif var == 3: #vz
-    #         dpar = np.zeros(27)
-    #         dpar[d] = 1
-    #         vv = np.stack([self.grid.evalBH(par = vx),self.grid.evalBH(par = vy),self.grid.evalBH(par = vz)],axis=2)
-    #         dv = np.stack([self.grid.evalBH(par = np.zeros(27)),self.grid.evalBH(par = np.zeros(27)),self.grid.evalBH(par = dpar)],axis = 2)
-    #         vb1 = np.stack([-vv[:,:,1],vv[:,:,0],vv[:,:,2]*0],axis=2)/np.sqrt(vv[:,:,0]**2 + vv[:,:,1]**2)[:,:,np.newaxis]
-    #         vb2 = np.stack([-vv[:,:,2]*vv[:,:,0],-vv[:,:,2]*vv[:,:,1],vv[:,:,0]**2 + vv[:,:,1]**2],axis=2)/np.sqrt(vv[:,:,2]**2*vv[:,:,0]**2 + vv[:,:,2]**2*vv[:,:,1]**2+(vv[:,:,0]**2 + vv[:,:,1]**2)**2)[:,:,np.newaxis]
-    #         ww = vb1*self.grid.evalBH(par = rho1)[:,:,np.newaxis] + vb2*self.grid.evalBH(par = rho2)[:,:,np.newaxis]
-    #         c1 = (vv[:,:,0]**2 + vv[:,:,1]**2)**(3.0/2.0)
-    #         c2 = (vv[:,:,2]**2*vv[:,:,0]**2 + vv[:,:,2]**2*vv[:,:,1]**2+(vv[:,:,0]**2 + vv[:,:,1]**2)**2)**(3.0/2.0)
-    #         dvb1 = np.stack([vv[:,:,2]*0,
-    #                          vv[:,:,2]*0,
-    #                          vv[:,:,2]*0],axis=2)*(dv[:,:,2]/c1)[:,:,np.newaxis]
-    #         dvb2 = np.stack([-vv[:,:,0]**5 - 2*vv[:,:,0]**3*vv[:,:,1]**2 - vv[:,:,0]*vv[:,:,1]**4,
-    #                          -vv[:,:,0]**4*vv[:,:,1] - 2*vv[:,:,0]**2*vv[:,:,1]**3 - vv[:,:,1]**5,
-    #                          -vv[:,:,2]*vv[:,:,0]**4 - 2*vv[:,:,2]*vv[:,:,0]**2*vv[:,:,1]**2 - vv[:,:,2]*vv[:,:,1]**4],axis=2)*(dv[:,:,2]/c2)[:,:,np.newaxis]
-    #         ww = vb1*self.grid.evalBH(par = rho1)[:,:,np.newaxis] + vb2*self.grid.evalBH(par = rho2)[:,:,np.newaxis]
-    #         dw = dvb1*self.grid.evalBH(par = rho1)[:,:,np.newaxis] + dvb2*self.grid.evalBH(par = rho2)[:,:,np.newaxis]
-    #         H = vv[:,:,:,np.newaxis]*dv[:,:,np.newaxis,:]  + dv[:,:,:,np.newaxis]*vv[:,:,np.newaxis,:] + dw[:,:,:,np.newaxis]*ww[:,:,np.newaxis,:] + ww[:,:,:,np.newaxis]*dw[:,:,np.newaxis,:]
-    #     elif var == 4: #rho1
-    #         dpar = np.zeros(27)
-    #         dpar[d] = 1
-    #         vv = np.stack([self.grid.evalBH(par = vx),self.grid.evalBH(par = vy),self.grid.evalBH(par = vz)],axis=2)
-    #         vb1 = np.stack([-vv[:,:,1],vv[:,:,0],vv[:,:,2]*0],axis=2)/np.sqrt(vv[:,:,0]**2 + vv[:,:,1]**2)[:,:,np.newaxis]
-    #         vb2 = np.stack([-vv[:,:,2]*vv[:,:,0],-vv[:,:,2]*vv[:,:,1],vv[:,:,0]**2 + vv[:,:,1]**2],axis=2)/np.sqrt(vv[:,:,2]**2*vv[:,:,0]**2 + vv[:,:,2]**2*vv[:,:,1]**2+(vv[:,:,0]**2 + vv[:,:,1]**2)**2)[:,:,np.newaxis]
-    #         ww = vb1*self.grid.evalBH(par = rho1)[:,:,np.newaxis] + vb2*self.grid.evalBH(par = rho2)[:,:,np.newaxis]
-    #         dw = vb1*self.grid.evalBH(par=dpar)[:,:,np.newaxis] 
-    #         H = ww[:,:,:,np.newaxis]*dw[:,:,np.newaxis,:] + dw[:,:,:,np.newaxis]*ww[:,:,np.newaxis,:]
-    #     elif var == 5: #rho2
-    #         dpar = np.zeros(27)
-    #         dpar[d] = 1
-    #         vv = np.stack([self.grid.evalBH(par = vx),self.grid.evalBH(par = vy),self.grid.evalBH(par = vz)],axis=2)
-    #         vb1 = np.stack([-vv[:,:,1],vv[:,:,0],vv[:,:,2]*0],axis=2)/np.sqrt(vv[:,:,0]**2 + vv[:,:,1]**2)[:,:,np.newaxis]
-    #         vb2 = np.stack([-vv[:,:,2]*vv[:,:,0],-vv[:,:,2]*vv[:,:,1],vv[:,:,0]**2 + vv[:,:,1]**2],axis=2)/np.sqrt(vv[:,:,2]**2*vv[:,:,0]**2 + vv[:,:,2]**2*vv[:,:,1]**2+(vv[:,:,0]**2 + vv[:,:,1]**2)**2)[:,:,np.newaxis]
-    #         ww = vb1*self.grid.evalBH(par = rho1)[:,:,np.newaxis] + vb2*self.grid.evalBH(par = rho2)[:,:,np.newaxis]
-    #         dw = vb2*self.grid.evalBH(par = dpar)[:,:,np.newaxis] 
-    #         H = ww[:,:,:,np.newaxis]*dw[:,:,np.newaxis,:] + dw[:,:,:,np.newaxis]*ww[:,:,np.newaxis,:]
-    #     return(H)
-
     def getH(self,gamma = None,vx = None,vy = None, vz = None,rho1 = None,rho2 = None,d=None,grad = False):
         if gamma is None and vx is None and vy is None and vz is None and rho1 is None and rho2 is None:
             gamma = self.gamma
@@ -343,6 +249,7 @@ class NonStatAnIso:
             dpar[d] = 1
             pg = np.exp(self.grid.evalBH(par = gamma))
             H_gamma = np.eye(3)*(np.stack([self.grid.bsH[:,:,d]*pg,self.grid.bsH[:,:,d]*pg,self.grid.bsH[:,:,d]*pg],axis=2)[:,:,:,np.newaxis])
+
             vv = np.stack([self.grid.evalBH(par = vx),self.grid.evalBH(par = vy),self.grid.evalBH(par = vz)],axis=2)
             vb1 = np.stack([-vv[:,:,1],vv[:,:,0],vv[:,:,2]*0],axis=2)/np.sqrt(vv[:,:,0]**2 + vv[:,:,1]**2)[:,:,np.newaxis]
             vb2 = np.stack([-vv[:,:,2]*vv[:,:,0],-vv[:,:,2]*vv[:,:,1],vv[:,:,0]**2 + vv[:,:,1]**2],axis=2)/np.sqrt(vv[:,:,2]**2*vv[:,:,0]**2 + vv[:,:,2]**2*vv[:,:,1]**2+(vv[:,:,0]**2 + vv[:,:,1]**2)**2)[:,:,np.newaxis]
@@ -452,48 +359,30 @@ class NonStatAnIso:
 
                 H_gamma, H_vx, H_vy, H_vz,H_rho1, H_rho2 = self.getH(gamma = par[27:54],vx = par[54:81], vy = par[81:108], vz = par[108:135],rho1=par[135:162],rho2=par[162:189],d=i,grad=True) 
 
-                #Hs_par = self.getH(gamma = par[27:54],vx = par[54:81], vy = par[81:108], vz = par[108:135],rho1=par[135:162],rho2=par[162:189],d=i,var = 0) 
                 A_par = - AH(self.grid.M,self.grid.N,self.grid.P,H_gamma,self.grid.hx,self.grid.hy,self.grid.hz)
                 Q_par = A_par.transpose()@self.iDv@A_mat +  A_mat.transpose()@self.iDv@A_par
                 g_par[27 + i] = 1/2*((Qinv - Qcinv)@Q_par).diagonal().sum()*self.r - 1/2*((mu_c.transpose()@Q_par)@mu_c).diagonal().sum()
 
-                #Hs_par = self.getH(gamma = par[27:54],vx = par[54:81], vy = par[81:108], vz = par[108:135],rho1=par[135:162],rho2=par[162:189],d=i,var = 1)
                 A_par = - AH(self.grid.M,self.grid.N,self.grid.P,H_vx,self.grid.hx,self.grid.hy,self.grid.hz)
                 Q_par = A_par.transpose()@self.iDv@A_mat +  A_mat.transpose()@self.iDv@A_par
                 g_par[54 + i] = 1/2*((Qinv - Qcinv)@Q_par).diagonal().sum()*self.r - 1/2*((mu_c.transpose()@Q_par)@mu_c).diagonal().sum()
                 
-                #Hs_par = self.getH(gamma = par[27:54],vx = par[54:81], vy = par[81:108], vz = par[108:135],rho1=par[135:162],rho2=par[162:189],d=i,var = 2)
                 A_par = - AH(self.grid.M,self.grid.N,self.grid.P,H_vy,self.grid.hx,self.grid.hy,self.grid.hz)
                 Q_par = A_par.transpose()@self.iDv@A_mat +  A_mat.transpose()@self.iDv@A_par
                 g_par[81 + i] = 1/2*((Qinv - Qcinv)@Q_par).diagonal().sum()*self.r - 1/2*((mu_c.transpose()@Q_par)@mu_c).diagonal().sum()
                   
-                #Hs_par = self.getH(gamma = par[27:54],vx = par[54:81], vy = par[81:108], vz = par[108:135],rho1=par[135:162],rho2=par[162:189],d=i,var = 3)
                 A_par = - AH(self.grid.M,self.grid.N,self.grid.P,H_vz,self.grid.hx,self.grid.hy,self.grid.hz)
                 Q_par = A_par.transpose()@self.iDv@A_mat +  A_mat.transpose()@self.iDv@A_par
                 g_par[108 + i] = 1/2*((Qinv - Qcinv)@Q_par).diagonal().sum()*self.r - 1/2*((mu_c.transpose()@Q_par)@mu_c).diagonal().sum()
                 
-                #Hs_par = self.getH(gamma = par[27:54],vx = par[54:81], vy = par[81:108], vz = par[108:135],rho1=par[135:162],rho2=par[162:189],d=i,var = 4)
                 A_par = - AH(self.grid.M,self.grid.N,self.grid.P,H_rho1,self.grid.hx,self.grid.hy,self.grid.hz)
                 Q_par = A_par.transpose()@self.iDv@A_mat +  A_mat.transpose()@self.iDv@A_par
                 g_par[135 + i] = 1/2*((Qinv - Qcinv)@Q_par).diagonal().sum()*self.r - 1/2*((mu_c.transpose()@Q_par)@mu_c).diagonal().sum()
                 
-                #Hs_par = self.getH(gamma = par[27:54],vx = par[54:81], vy = par[81:108], vz = par[108:135],rho1=par[135:162],rho2=par[162:189],d=i,var = 5)
                 A_par = - AH(self.grid.M,self.grid.N,self.grid.P,H_rho2,self.grid.hx,self.grid.hy,self.grid.hz)
                 Q_par = A_par.transpose()@self.iDv@A_mat +  A_mat.transpose()@self.iDv@A_par
                 g_par[162 + i] = 1/2*((Qinv - Qcinv)@Q_par).diagonal().sum()*self.r  - 1/2*((mu_c.transpose()@Q_par)@mu_c).diagonal().sum()
                 
-                #for j in range(self.r):
-                #    g_par[i] = g_par[i] + (- 1/2*mu_c[:,j].transpose()@Q_kappa@mu_c[:,j])
-                #    g_par[27 + i] = g_par[27 + i] + (- 1/2*mu_c[:,j].transpose()@Q_gamma@mu_c[:,j])
-                #    g_par[54 + i] = g_par[54 + i] + (- 1/2*mu_c[:,j].transpose()@Q_vx@mu_c[:,j])
-                #    g_par[81 + i] = g_par[81 + i] + (- 1/2*mu_c[:,j].transpose()@Q_vy@mu_c[:,j])
-                #    g_par[108 + i] = g_par[108 + i] + (- 1/2*mu_c[:,j].transpose()@Q_vz@mu_c[:,j])
-                #    g_par[135 + i] = g_par[135 + i] + (- 1/2*mu_c[:,j].transpose()@Q_rho1@mu_c[:,j])
-                #    g_par[162 + i] = g_par[162 + i] + (- 1/2*mu_c[:,j].transpose()@Q_rho2@mu_c[:,j])
-                #    if i == 0:
-                #        g_par[189] = g_par[189] + (- 1/2*(data[:,j] - self.S@mu_c[:,j]).transpose()@(data[:,j] - self.S@mu_c[:,j])*np.exp(par[189]))
-                #        like = like + (- 1/2*mu_c[:,j].transpose()@Q@mu_c[:,j] - np.exp(par[189])/2*(data[:,j] - self.S@mu_c[:,j]).transpose()@(data[:,j]-self.S@mu_c[:,j]))
-
             like =  -like/(self.S.shape[0]*self.r)
             self.likediff = like - self.like
             self.like = like
@@ -502,7 +391,7 @@ class NonStatAnIso:
             if self.verbose:
                 if self.truth is not None:
                     print("# %4.0f"%self.opt_steps," log-likelihood = %4.4f"%(-like), "\u03BA = %2.2f"%(np.mean(par[0:27]-self.truth[:27])), "\u03B3 = %2.2f"%(np.mean(par[27:54]-self.truth[27:54])),"vx = %2.2f"%(np.mean(np.abs(par[54:81])-np.abs(self.truth[54:81]))),"vy = %2.2f"%(np.mean(np.abs(par[81:108])-np.abs(self.truth[81:108]))),
-                    "vz = %2.2f"%(np.mean(np.abs(par[108:135])-np.abs(self.truth[108:135]))),"\u03C1_1 = %2.2f"%(np.mean(par[135:162]-self.truth[135:162])),"\u03C1_2 = %2.2f"%(np.mean(par[162:189]-self.truth[162:189])), "\u03C3 = %2.2f"%(np.mean(par[189]-self.truth[189])))
+                    "vz = %2.2f"%(np.mean(np.abs(par[108:135])-np.abs(self.truth[108:135]))),"\u03C1_1 = %2.2f"%(np.mean(par[135:162]-self.truth[135:162])),"\u03C1_2 = %2.2f"%(np.mean(par[162:189]-self.truth[162:189])), "\u03C3 = %2.2f"%(par[189]-self.truth[189]))
                 else:  
                     print("# %4.0f"%self.opt_steps," log-likelihood = %4.4f"%(-like))#, "\u03BA = %2.2f"%np.exp(par[0]), "\u03B3 = %2.2f"%np.exp(par[1]), "\u03C3 = %2.2f"%np.sqrt(1/np.exp(par[2])))
             return((like,jac))
@@ -510,7 +399,7 @@ class NonStatAnIso:
             like = 1/2*Q_fac.logdet()*self.r + self.S.shape[0]*self.r*par[189]/2 - 1/2*Q_c_fac.logdet()*self.r
             for j in range(self.r):
                 like = like + (- 1/2*mu_c[:,j].transpose()@Q@mu_c[:,j] - np.exp(par[189])/2*(data[:,j] - self.S@mu_c[:,j]).transpose()@(data[:,j]-self.S@mu_c[:,j]))
-            like = like/(self.S.shape[0]*self.r)
+            like = -like/(self.S.shape[0]*self.r)
             self.like = like
             self.opt_steps = self.opt_steps + 1
             if self.verbose:
