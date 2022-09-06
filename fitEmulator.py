4a3878da7af99aa60e458940db5eea55af2ce447
berild <moutzebe@gmail.com>
2022-09-06 11:51:18 +0200

    add different mean  fit

--- fitEmulator.py
+++ fitEmulator.py
@@ -4,13 +4,14 @@ import sys
 
 def main(argv):
     mis = mission(argv[0])
-    mis.emulator(model=int(argv[1]))
+    mis.emulator(model=int(argv[1]),mean = int(argv[2]))
     if mis.muf is None:
         mis.mean(version2 = "tfc")
     if int(argv[1])==4:
         par = np.hstack([[-1.1]*27,[-1.2]*27,[5.1]*27,[4.1]*27,[0.04]*27,[0.4]*27,[0.6]*27,5])
     elif int(argv[1]) == 2:
         par = np.array([-1.1,-1.2,5.1,4.1,0.04,0.4,0.6,5])
+    end = ['_new','_lag','_avr']
     mis.fit(par= par, verbose = True)
     print("finished")
 
