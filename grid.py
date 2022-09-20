aed3028aa4ccf8c247bcd1d5578d97afe75a7119
berild <moutzebe@gmail.com>
2022-09-20 09:12:20 +0200

    fix boundary of grid

--- grid.py
+++ grid.py
@@ -60,9 +60,12 @@ class Grid:
             ymax = self.sy.max()
             zmin = self.sz.min()
             zmax = self.sz.max()
-        kx = np.linspace(xmin - (xmax- xmin)/np.sqrt(16/d)*d,xmax+(xmax-xmin)/np.sqrt(16/d)*d,d+6)
-        ky = np.linspace(ymin - (ymax- ymin)/np.sqrt(16/d)*d,ymax+(ymax-ymin)/np.sqrt(16/d)*d,d+6)
-        kz = np.linspace(zmin - (zmax- zmin)/np.sqrt(16/d)*d,zmax+(zmax-zmin)/np.sqrt(16/d)*d,d+6)
+        kx = np.linspace(xmin - 2*(xmax-xmin)/3, xmax + 2*(xmax-xmin)/3,8)
+        ky = np.linspace(ymin - 2*(ymax-ymin)/3, ymax + 2*(ymax-ymin)/3,8)
+        kz = np.linspace(zmin - 2*(zmax-zmin)/3, zmax + 2*(zmax-zmin)/3,8)
+        #kx = np.linspace(xmin - (xmax- xmin)/np.sqrt(16/d)*d,xmax+(xmax-xmin)/np.sqrt(16/d)*d,d+6)
+        #ky = np.linspace(ymin - (ymax- ymin)/np.sqrt(16/d)*d,ymax+(ymax-ymin)/np.sqrt(16/d)*d,d+6)
+        #kz = np.linspace(zmin - (zmax- zmin)/np.sqrt(16/d)*d,zmax+(zmax-zmin)/np.sqrt(16/d)*d,d+6)
         Bx = list([np.stack([((tx >= kx[i])&(tx < kx[i+1]) | ((tx >= kx[i])&(tx <= kx[i+1])&(i==(kx.size-2))))*1.0 for i in range(kx.size-1)],axis=1)])
         By = list([np.stack([((ty >= ky[i])&(ty < ky[i+1]) | ((ty >= ky[i])&(ty <= ky[i+1])&(i==(ky.size-2))))*1.0 for i in range(ky.size-1)],axis=1)])
         Bz = list([np.stack([((tz >= kz[i])&(tz < kz[i+1]) | ((tz >= kz[i])&(tz <= kz[i+1])&(i==(kz.size-2))))*1.0 for i in range(kz.size-1)],axis=1)])
@@ -74,21 +77,24 @@ class Grid:
                 Bx[r][:,i] = (tx - kx[i])/(kx[i+r]-kx[i])*Bx[r-1][:,i] + (kx[i+r+1] - tx)/(kx[i+r+1]-kx[i+1])*Bx[r-1][:,i+1]
                 By[r][:,i] = (ty - ky[i])/(ky[i+r]-ky[i])*By[r-1][:,i] + (ky[i+r+1] - ty)/(ky[i+r+1]-ky[i+1])*By[r-1][:,i+1]
                 Bz[r][:,i] = (tz - kz[i])/(kz[i+r]-kz[i])*Bz[r-1][:,i] + (kz[i+r+1] - tz)/(kz[i+r+1]-kz[i+1])*Bz[r-1][:,i+1]
-        bx = Bx[d]
-        by = By[d]
-        bz = Bz[d]
-        bx = np.delete(bx,1,1)
-        by = np.delete(by,1,1)
-        bz = np.delete(bz,1,1)
-        bx = np.delete(bx,Bx[d].shape[1]-2,1)
-        by = np.delete(by,By[d].shape[1]-2,1)
-        bz = np.delete(bz,Bz[d].shape[1]-2,1)
-        bx[:,0] = (Bx[d][:,0] + Bx[d][:,1])
-        by[:,0] = (By[d][:,0] + By[d][:,1])
-        bz[:,0] = (Bz[d][:,0] + Bz[d][:,1])
-        bx[:,Bx[d].shape[1]-3] = (Bx[d][:,Bx[d].shape[1]-1] + Bx[d][:,Bx[d].shape[1]-2])
-        by[:,By[d].shape[1]-3] = (By[d][:,By[d].shape[1]-1] + By[d][:,By[d].shape[1]-2])
-        bz[:,Bz[d].shape[1]-3] = (Bz[d][:,Bz[d].shape[1]-1] + Bz[d][:,Bz[d].shape[1]-2])
+        bx = np.stack([Bx[2][:,0] + Bx[2][:,1],Bx[2][:,2],Bx[2][:,3] + Bx[2][:,4]],axis = 1)
+        by = np.stack([By[2][:,0] + By[2][:,1],By[2][:,2],By[2][:,3] + By[2][:,4]],axis = 1)
+        bz = np.stack([Bz[2][:,0] + Bz[2][:,1],Bz[2][:,2],Bz[2][:,3] + Bz[2][:,4]],axis = 1)
+        #bx = Bx[d]
+        #by = By[d]
+        #bz = Bz[d]
+        #bx = np.delete(bx,1,1)
+        #by = np.delete(by,1,1)
+        #bz = np.delete(bz,1,1)
+        #bx = np.delete(bx,Bx[d].shape[1]-2,1)
+        #by = np.delete(by,By[d].shape[1]-2,1)
+        #bz = np.delete(bz,Bz[d].shape[1]-2,1)
+        #bx[:,0] = (Bx[d][:,0] + Bx[d][:,1])
+        #by[:,0] = (By[d][:,0] + By[d][:,1])
+        #bz[:,0] = (Bz[d][:,0] + Bz[d][:,1])
+        #bx[:,Bx[d].shape[1]-3] = (Bx[d][:,Bx[d].shape[1]-1] + Bx[d][:,Bx[d].shape[1]-2])
+        #by[:,By[d].shape[1]-3] = (By[d][:,By[d].shape[1]-1] + By[d][:,By[d].shape[1]-2])
+        #bz[:,Bz[d].shape[1]-3] = (Bz[d][:,Bz[d].shape[1]-1] + Bz[d][:,Bz[d].shape[1]-2])
         return(bx,by,bz)
 
     def basisN(self):
